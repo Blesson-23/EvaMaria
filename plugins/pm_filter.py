d56bbf830fd05af251f782a6b62dd79c8bf60233
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2022-02-07 20:55:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -442,7 +442,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.send_message(
             text=script.ABOUT_TXT,
             chat_id=query.message.chat.id,
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
             parse_mode='html'
         )
     elif query.data == "source":
