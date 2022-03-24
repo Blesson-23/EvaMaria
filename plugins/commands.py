b425f4c6f62a161189eadfdab1b22efffd401500
Blesson-23 <94548940+Blesson-23@users.noreply.github.com>
2022-03-24 15:10:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('👑 Updates', url='https://t.me/TeamEvamaria')
+                InlineKeyboardButton('👑 Updates', url='https://t.me/xdsupport')
             ],
             [
                 InlineKeyboardButton('🍂 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -74,7 +74,7 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             kk, file_id = message.command[1].split("_", 1)
             pre = 'checksubp' if kk == 'filep' else 'checksub' 
-            btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+            btn.append([InlineKeyboardButton("ഒന്നുടെ try ചെയ്യു 🥺", callback_data=f"{pre}#{file_id}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
