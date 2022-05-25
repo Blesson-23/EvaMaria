3604471f4e7509de763219d2d631cf607b235f5d
Blesson-23 <94548940+Blesson-23@users.noreply.github.com>
2022-05-25 22:39:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -460,12 +460,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "start":
         await query.message.delete()
         buttons = [[
-            InlineKeyboardButton('🍌 Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('💣Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('Search🍌', switch_inline_query_current_chat='')
+            InlineKeyboardButton('Search🔎', switch_inline_query_current_chat='')
         ], [
-            InlineKeyboardButton('🍌Help', callback_data='help'),
-            InlineKeyboardButton('🍌ᴀʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🤸‍♀️Help', callback_data='help'),
+            InlineKeyboardButton('💜ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.send_sticker(
@@ -491,10 +491,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         await query.message.delete()
         buttons = [[
-            InlineKeyboardButton('🍌ᴀʙᴏᴜᴛ', callback_data='button')
+            InlineKeyboardButton('💣ᴀʙᴏᴜᴛ', callback_data='button')
             ],[
-            InlineKeyboardButton('🍌Home', callback_data='start'),
-            InlineKeyboardButton('🍌Close', callback_data='close_data')
+            InlineKeyboardButton('💣Home', callback_data='start'),
+            InlineKeyboardButton('🚪Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.send_sticker(
