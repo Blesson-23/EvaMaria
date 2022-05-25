5ab2cec4e24e82fc651e7db309af1d55dcf765ed
Blesson-23 <94548940+Blesson-23@users.noreply.github.com>
2022-05-25 22:26:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -469,7 +469,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.send_sticker(
-            sticker="CAACAgIAAxkBAAEP919ijlzVF0pID4jnuSpPWnHmY_V95AACuRcAAjCPUUmWMTQKtDsdIyQE",
+            sticker="CAACAgIAAxkBAAEP95Zijl-IAAEj2QyY0ovnd6wbofCy8NUAAvYSAAIp_UhJZrzas7gxByokBA",
             chat_id=query.message.chat.id,
             reply_markup=reply_markup
            # parse_mode='html'
