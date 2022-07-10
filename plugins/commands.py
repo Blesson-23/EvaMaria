6821911acc7ed31032b53aeefb5632bf62be54b9
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2022-07-10 17:55:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,6 +27,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 aiohttpsession = ClientSession()
+API = "https://apis.xditya.me/lyrics?song="
 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
@@ -574,3 +575,32 @@ async def carbon_func(_, message):
         photo=carbon)
     await m.delete()
     carbon.close()
+
+@Client.on_message(filters.text & filters.command(["lyrics"]))
+async def sng(bot, message):
+        if not message.reply_to_message:
+          await message.reply_text("Please reply to a message")
+        else:          
+          mee = await message.reply_text("```Searching 🔎```")
+          song = message.reply_to_message.text
+          chat_id = message.from_user.id
+          rpl = lyrics(song)
+          await mee.delete()
+          try:
+            await mee.delete()
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id)
+          except Exception as e:                            
+             await message.reply_text(f"I Can't Find A Song With ```{song}```", quote = True)
+
+
+
+def search(song):
+        r = requests.get(API + song)
+        find = r.json()
+        return find
+       
+def lyrics(song):
+        fin = search(song)
+        text = f'🎶 Successfully Extracted Lyrics Of {song} 🎶\n\n'
+        text += f'`{fin["lyrics"]}`'
+        return text
