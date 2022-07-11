a5fe41a78c0ceea609894e11f696509e89a5d193
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2022-07-11 23:07:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,16 +1,9 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """🙋🏻‍♂️   Hellooo   {}
+    HELP_TXT = """**🙋🏻‍♂️   Hellooo   {}**
+**Avilable all Modules Here**
 
-○ Available Commands
-     
- /start - Check I'm Alive..
-
-
-
-
-- Works in bot pm
 ©️MᴀɪɴᴛᴀɪɴᴇD Bʏ <a href=http://t.me/blesson_3> ᴍʀ ᴍᴀᴋʀɪ [ᴏɴʟɪɴᴇ]</a>"""
 
 
