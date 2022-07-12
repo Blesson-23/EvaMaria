64bae29ab934dc1cf637f9b1d03f7fb17a31a1ff
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2022-07-12 09:57:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,6 +8,7 @@ import re
 import json
 import base64
 import sys
+
 from aiohttp import ClientSession
 from asyncio import get_running_loop
 from io import BytesIO
@@ -29,6 +30,115 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 aiohttpsession = ClientSession()
 API = "https://apis.xditya.me/lyrics?song="
+
+Langs ="""**Available Languages**
+    `'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu'`"""
 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
@@ -605,3 +715,31 @@ def lyrics(song):
         text = f'🎶 Successfully Extracted Lyrics Of {song} 🎶\n\n'
         text += f'`{fin["lyrics"]}`'
         return text
+
+# Translation
+
+@Client.on_message(filters.command("tr"))
+async def tr(_, message):
+    if len(message.command) != 2:
+        await message.reply_text("/tr [LANGUAGE_CODE]")
+        return
+    lang = message.text.split(None, 1)[1]
+    if not message.reply_to_message or not lang:
+        await message.reply_text(
+            "Reply to a message with /tr [language code]"
+            + "\n"Langs
+        )
+        return
+    if message.reply_to_message.text:
+        text = message.reply_to_message.text
+        try:
+            i = Translator().translate(text, dest=lang)
+        except ValueError:
+            await message.reply_text("[ERROR]: LANGUAGE NOT FOUND")
+            return
+        await message.reply_text(i.text)
+    elif message.reply_to_message.caption:
+        text = message.reply_to_message.caption
+        i = Translator().translate(text, dest=lang)
+        await message.reply_text(i.text)
+
